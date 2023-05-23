22bcb10046bfae212c2f101ab8854a4ab4b46225
khemraj regmi <khemrajregmi@khemrajs-mbp.localdomain>
2023-05-23 22:41:42 +0200

    add checkWinner method to the tictactoe class

--- tic-tac.teo.php
+++ tic-tac.teo.php
@@ -30,6 +30,50 @@ class TicTacToe
         $this->board[$row][$col] = $player;
         $this->currentPlayer = ($player === 'X') ? 'O' : 'X';
     }
+
+    public function checkWinner()
+    {
+        // Check the matching rows
+        for ($i = 0; $i < 3; $i++) {
+            if (
+                $this->board[$i][0] !== '-' &&
+                $this->board[$i][0] === $this->board[$i][1] &&
+                $this->board[$i][0] === $this->board[$i][2]
+            ) {
+                return $this->board[$i][0];
+            }
+        }
+
+        // Check the matching columns
+        for ($i = 0; $i < 3; $i++) {
+            if (
+                $this->board[0][$i] !== '-' &&
+                $this->board[0][$i] === $this->board[1][$i] &&
+                $this->board[0][$i] === $this->board[2][$i]
+            ) {
+                return $this->board[0][$i];
+            }
+        }
+
+        // Check the matching diagonals
+        if (
+            $this->board[0][0] !== '-' &&
+            $this->board[0][0] === $this->board[1][1] &&
+            $this->board[0][0] === $this->board[2][2]
+        ) {
+            return $this->board[0][0];
+        }
+
+        if (
+            $this->board[0][2] !== '-' &&
+            $this->board[0][2] === $this->board[1][1] &&
+            $this->board[0][2] === $this->board[2][0]
+        ) {
+            return $this->board[0][2];
+        }
+
+        return null;
+    }
 }
 
 class TicTacToeCLI
